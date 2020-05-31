37fb5f97da94175dc8cae4bc6a2eed36476ea089
Sofia <sofiasimas1992@gmail.com>
2020-05-31 13:06:56 -0300

    add login page

---
+++ public/pages/login/template.js
@@ -0,0 +1,13 @@
+const template = `
+<h2>Página login</h2>
+<a href="#register">Cadastrar</a>
+<div id="message">
+      <h2>Welcome</h2>
+      <h1>Firebase Hosting Setup Complete</h1>
+      <p>You're seeing this because you've successfully setup Firebase Hosting. Now it's time to go build something extraordinary!</p>
+      <a target="_blank" href="https://firebase.google.com/docs/hosting/">Open Hosting Documentation</a>
+</div>
+<div id="firebaseui-auth-container">
+</div>
+`;
+export default template;
