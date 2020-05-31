32f7957551d49917542e6a5c1566cc7ab0676e97
Sofia <sofiasimas1992@gmail.com>
2020-05-31 13:06:23 -0300

    add router

---
+++ public/router/routes.js
@@ -0,0 +1,9 @@
+import login from '../pages/login/index.js';
+import register from '../pages/register/index.js';
+
+const routes = {
+    login: login(), 
+    register: register()
+}
+
+export default routes;
