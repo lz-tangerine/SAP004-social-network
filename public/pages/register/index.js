c46adfd3ecd65f37a24dc593d5cdd0ce170cbaf4
Sofia <sofiasimas1992@gmail.com>
2020-06-02 09:19:10 -0300

    add register page

--- public/pages/register/index.js
+++ public/pages/register/index.js
@@ -1,11 +1,20 @@
 import template from './template.js'
+import {createUser} from './data.js'
 
-const register = ()  => {
+const render = ()  => {
     const container = document.createElement('div');
 
     container.innerHTML = template;
-
     return container;
 }
 
-export default register;
+const init = () => {
+    const registerButton = document.getElementById('register');
+    console.log(registerButton)
+    registerButton.addEventListener('click', createUser);
+}
+
+export default {
+    render, 
+    init,
+}
--- public/pages/register/template.js
+++ public/pages/register/template.js
@@ -1,5 +1,10 @@
 const template = `
-<h2>Página de cadastro</h2>
+<h2>Página de teste</h2>
+<label for="email">Email</label>
+<input type="text" id="email" />
+<label for="password">Senha</label>
+<input type="password" id="password"/>
+<button id="register">Cadastrar</button>
 <a href="#">Voltar para o login</a>
 `;
 
