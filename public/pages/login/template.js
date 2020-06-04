d93ba1206df825d34e72d52eda62d553558c0820
Sofia <sofiasimas1992@gmail.com>
2020-06-04 08:46:42 -0300

    update login

--- public/pages/login/template.js
+++ public/pages/login/template.js
@@ -1,29 +1,24 @@
 const template = `
-<div id="message" class="container">
-  <section>
-    <section class="login">
-        <img alt="logo" class="control-logo" src="imagens/logo.png"/>
-        <img alt="logo-name" class="name-logo" src="imagens/logo-name.png"/>
+  <section class="login container">
+      <img alt="logo" class="control-logo" src="imagens/logo.png"/>
+      <img alt="logo-name" class="name-logo" src="imagens/logo-name.png"/>
+      <div>
         <label for="email" class="email-login">email</label>
         <input type="text" id="email" name="email-login" class="email-input">
+      </div>
+      <div>
         <label for="password" class="password-login">senha</label>
         <input type="password" id="password" name="password" class="password-input">
-        <span id="loginError"></span>
-        <button id="login" class="button-log-in">entrar</button>
-        <p>entrar com</p>
-        <button id="loginFacebook" class="button">
-        <img src="imagens/icons-facebook.png" class="login-facebook"/>
-        </button>
-        <button id="loginGoogle" class="button">
-          <img src="imagens/icons-google-01.png" class="login-google"/>
-        </button>
-        <section>
-          <p>Não tem uma conta? </p>
-          <a href="#register">criar conta</a>
-        <section>
-    <section>
+      </div>
+      <span id="loginError"></span>
+      <button id="login" class="button-log-in">entrar</button>
+      <p>entrar com</p>
+      <div class="img-social">
+        <img src="imagens/icons-facebook.png" id="loginFacebook" class="login-facebook"/>
+        <img src="imagens/icons-google-01.png" id="loginGoogle" class="login-google"/>
+      </div>
+      <p>Não tem uma conta?</p>
+      <a href="#register" class="link-create">criar conta</a>
   </section>
-
-</div>
 `;
 export default template;
