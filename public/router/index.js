81c0afaab8b23b97cf551b408d15e09a64dd9108
Carol Moscheta <carolina.moscheta@gmail.com>
2020-06-05 16:25:13 -0300

    configuração do firebase

--- public/router/index.js
+++ public/router/index.js
@@ -2,27 +2,26 @@ import routes from './routes.js';
 
 const pageContainer = document.getElementById('page-container');
 
-const validateHash = (hash) => hash === '' ? 'login' : hash.replace('#', '');
-
-const init = () => {
-    window.addEventListener('hashchange', () => {
-        renderPage();
-    })
-}
+const validateHash = hash => (hash === '' ? 'login' : hash.replace('#', ''));
 
 const renderPage = () => {
-    pageContainer.innerHTML = '';
-    const page = validateHash(window.location.hash);
-    pageContainer.appendChild(routes[page]);
-}
+  pageContainer.innerHTML = '';
+  const page = validateHash(window.location.hash);
+  pageContainer.appendChild(routes[page]);
+};
+
+const init = () => {
+  window.addEventListener('hashchange', () => {
+    renderPage();
+  });
+};
 
 const router = () => {
-    window.addEventListener('load', () => {
-        renderPage();
-    })
-    init();
-}
+  window.addEventListener('load', () => {
+    renderPage();
+  });
+  init();
+};
 
+// eslint-disable-next-line eol-last
 export default router;
-
-a vida é BeforeUnloadEvent
--- public/router/routes.js
+++ public/router/routes.js
@@ -2,8 +2,9 @@ import login from '../pages/login/index.js';
 import register from '../pages/register/index.js';
 
 const routes = {
-    login: login(), 
-    register: register()
-}
+  login: login(),
+  register: register(),
+};
 
+// eslint-disable-next-line eol-last
 export default routes;
