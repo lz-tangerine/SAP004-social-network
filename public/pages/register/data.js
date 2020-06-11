8e63f5fd1c1557d94d22f1bb9a40ebbcaa4b4380
Sofia <sofiasimas1992@gmail.com>
2020-06-11 08:38:38 -0300

    remove data

--- public/pages/register/data.js
+++
@@ -1,7 +0,0 @@
-export const createUser = () => {
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    firebase.auth().createUserWithEmailAndPassword(email, password).then(() => {
-        window.location = '#';
-    })
-}
