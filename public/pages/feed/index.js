48e323935f9f9eedea9c1fed562193c70c6c367d
Sofia <sofiasimas1992@gmail.com>
2020-06-11 08:37:49 -0300

    add feed

--- public/pages/feed/index.js
+++ public/pages/feed/index.js
@@ -1,4 +1,5 @@
 import template from './template.js'
+import postTemplate from './postTemplate.js'
 
 const render = ()  => {
     const container = document.createElement('div');
@@ -8,9 +9,37 @@ const render = ()  => {
     return container;
 }
 
+const createPost = () => {
+    const text = document.getElementById('postText').value
+    const userId = firebase.auth().currentUser.uid
+    const post = {
+        text,
+        userId,
+        likes:0,
+    }
+    firebase.firestore().collection('posts').add(post);
+}
+
+const showFeed = () => {
+    const timeline = document.getElementById('timeline')
+    firebase.firestore().collection("posts")
+    .onSnapshot(function(querySnapshot) {
+        var posts = [];
+        querySnapshot.forEach(function(doc) {
+            posts.push(doc.data());
+        });
+        
+        const content = posts.map(postTemplate)
+        timeline.innerHTML = content;
+    });
+}
+    
 
 const init =  () => {
     console.log('incializaou')
+    const postButton = document.getElementById('createPost');
+    postButton.addEventListener('click', createPost);
+    showFeed();
     
 }
 
--- public/pages/feed/template.js
+++ public/pages/feed/template.js
@@ -1,4 +1,66 @@
 const template = `
-<h2>Página feed</h2>
+<section class="login container">
+    <header class="header">
+        <div>
+            <input id="menu-hamburguer" type="checkbox" class="menu-btn"/>
+            <label for="menu-hamburguer" class="menu-icon"><span class="nav-icon"></span></label>
+            <ul class="menu">
+                <li><a href="#perfil" class="menu-item">Perfil</a></li>
+                <li><a href="#páginas" class="menu-item">Páginas</a></li>
+                <li><a href="#grupos" class="menu-item">Grupos</a></li>
+                <li><a href="#calendário" class="menu-item">Calendário</a></li>
+                <li><a href="#chat" class="menu-item">Chat</a></li>
+                <li><a href="#sair" class="menu-item">Sair</a></li>
+            </ul>
+        </div>
+        <div>
+            <img alt="logo-nav" class="name-logo" src="imagens/logo-name.png"/>
+        </div>
+    </header>
+
+    <main class="feed-post">
+        <img src="imagens/astronautrosie.jpg" alt="" class="foto-perfil">
+        <div class="name-perfil">
+            <p id="userId" class="user-perfil">Player 1</p>
+            <p id="typeGame" class="type-person">Jogos Plataforma</p>
+        </div>
+        <section class="post-feed">
+            <textarea type="text" id="postText" name="postText" class="post"></textarea> 
+            <div class="button-post">
+                <img src="imagens/foto.png" class="img-photo"/>
+                <button id="createPost" class="play">Play!</button>
+            </div>
+        </section>
+    </main>
+
+        <section class="feed">
+            <div class="feed-pessoal">
+                <img src="imagens/astronautrosie.jpg" alt="" class="foto-feed">
+                <div class="name-perfil">
+                    <p id="userId" class="user-post">Player 1</p>
+                </div>
+            </div>
+            <div id="timeline"></div>
+                <img src="imagens/like-01.png" id="like" class="like-feed"/>
+                <img src="imagens/comentar.png" id="comentar" class="comentar-feed"/>
+                <img src="imagens/calendar.png" id="calendar" class="calendar-feed"/>]
+                <img src="imagens/compartilhar.png" id="compartilhar" class="compartilhar-feed"/>
+            </div>
+        </section>
+
+        <section class="post-like">
+            <div class="feed-outher">
+                    <img src="imagens/astronautrosie.jpg" alt="" class="foto-feed">
+                    <div class="name-perfil">
+                        <p id="userId" class="user-outher">Player 2</p>
+                    </div>
+            </div>
+                <textarea id="story" name="story" rows="5" cols="33">Estou pensando...</textarea>
+            <div>
+                <img alt="like" class="like" src="imagens/like-01.png"/>
+                <button id="compartilhar" class="compart">compartilhar</button>
+            </div>
+        </section>
+</section>
 `;
 export default template;
