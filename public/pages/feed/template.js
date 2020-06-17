da69a011fd577515dddcf49247335bef22113439
Karina Pereira de Souza <karina.souza1981@gmail.com>
2020-06-17 11:05:11 -0300

    profile

--- public/pages/feed/template.js
+++ public/pages/feed/template.js
@@ -1,47 +1,46 @@
 const template = `
 <section class="feed-container">
-    <header class="header">
-        <nav id="menu-feed">
-            <input id="menu-hamburguer" type="checkbox" class="menu-btn"/>
-            <label for="menu-hamburguer" class="menu-icon"><span class="nav-icon"></span></label>
-            <ul class="menu">
-                <li><a href="#perfil" class="menu-item">Perfil</a></li>
-                <li><a href="#feed" class="menu-item">Feed</a></li>
-                <li><a href="#chat" class="menu-item">Chat</a></li>
-                <li><a href="#sair" class="menu-item">Sair</a></li>
-            </ul>
-        </nav>
-        <div>
-        <img alt="logo-nav" class="feed-logo" src="imagens/logo-name.png"/>
-        </div>
+  <header class="header">
+    <nav id="menu-feed">
+      <input id="menu-hamburguer" type="checkbox" class="menu-btn"/>
+      <label for="menu-hamburguer" class="menu-icon"><span class="nav-icon"></span></label>
+      <ul class="menu">
+        <li><a href="#perfil" class="menu-item">Perfil</a></li>
+        <li><a href="#feed" class="menu-item">Feed</a></li>
+        <li><a href="#chat" class="menu-item">Chat</a></li>
+        <li><a href="#login" class="menu-item">login</a></li>
+      </ul>
+    </nav>
+    <div>
+    <img alt="logo-nav" class="feed-logo" src="imagens/logo-name.png"/>
+    </div>
     </header>
 
-    <main class="feed-post">
-        <section class="photo-name">
-            <img alt="capa" class="capa-feed" src="imagens/fundo.jpg"/>
-            <img src="imagens/astronautrosie.jpg" alt="" class="foto-perfil">
-            <div class="name-perfil">
-                <p id="userId" class="user-perfil">Player 1</p>
-                <p id="typeGame" class="type-person">Jogos Plataforma</p>
-            </div>
-        </section>
-        <section class="post-feed">
-            <div class="switch__container">
-                <input id="private" class="switch switch--shadow" type="checkbox" />
-                <label for="private" class="private-button"></label>
-                <p class="post-private">Privado</p>
-            </div>
-            <textarea type="text" id="postText" name="postText" class="post"></textarea> 
-            <div class="button-post">
-                <img src="imagens/foto.png" class="img-photo"/>
-                <button id="createPost" class="play">Compartilhar</button>
-            </div>
-        </section>
-    </main>
-
-        <section class="feed">
-            <div id="timeline" class="timeline-feed"></div>
-        </section>
+  <main class="feed-post">
+    <section class="photo-name">
+      <img alt="capa" class="capa-feed" src="imagens/fundo.jpg"/>
+      <img src="imagens/astronautrosie.jpg" alt="" class="foto-perfil">
+      <div class="name-perfil">
+        <p id="userId" class="user-perfil">Player 1</p>
+        <p id="typeGame" class="type-person">Jogos Plataforma</p>
+      </div>
+  </section>
+  <section class="post-feed">
+      <div class="switch__container">
+        <input id="private" class="switch switch--shadow" type="checkbox" />
+        <label for="private" class="private-button"></label>
+        <p class="post-private">Privado</p>
+      </div>
+      <textarea type="text" id="postText" name="postText" class="post"></textarea> 
+      <div class="button-post">
+        <img src="imagens/foto.png" class="img-photo"/>
+        <button id="createPost" class="play">Compartilhar</button>
+      </div>
+    </section>
+  </main>
+  <section class="feed">
+      <div id="timeline" class="timeline-feed"></div>
+    </section>
 </section>
 `;
 export default template;
--- public/pages/register/index.js
+++ public/pages/register/index.js
@@ -1,60 +1,60 @@
 import template from './template.js'
 
 const saverUserData = () => {
-    const name = document.getElementById('name');
-    const surname = document.getElementById('surname');
-    const status = document.getElementById('status');
-    const date = document.getElementById('date');
-    const user = {
-      name: name.value,
-      surname: surname.value,
-      status: status.value,
-      date: date.value,
-      user_uid: firebase.auth().currentUser.uid,
-    };
-    firebase.firestore().collection('users').add(user);
+  const name = document.getElementById('name');
+  const surname = document.getElementById('surname');
+  const status = document.getElementById('status');
+  const date = document.getElementById('date');
+  const user = {
+    name: name.value,
+    surname: surname.value,
+    status: status.value,
+    date: date.value,
+    user_uid: firebase.auth().currentUser.uid,
   };
+  firebase.firestore().collection('users').add(user);
+};
 
 const create = () => {
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    const passwordConfirm = document.getElementById('password-confirm').value;
-    const name = document.getElementById('name').value;
-    const surname = document.getElementById('surname').value;
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  const passwordConfirm = document.getElementById('password-confirm').value;
+  const name = document.getElementById('name').value;
+  const surname = document.getElementById('surname').value;
 
-    if (name === '' || surname === '' || email === '' || password === '') {
-      loginError.innerHTML = 'Preencha os campos em branco';
-    } else if (password !== passwordConfirm) {
-      loginError.innerHTML = 'Senha inválida';
-    } else if (password.length < 6) {
-      loginError.innerHTML = 'A senha deve possuir ao menos 6 caracteres';
-    } else if (password === passwordConfirm) {
-      firebase.auth().createUserWithEmailAndPassword(email, password)
-        .then((teste) => {
-          console.log(teste);
-          saverUserData();
-          window.location = '#login';
-        }).catch((error) => {
-          const errorCode = error.code;
-          console.log(error);
-          
-        });
-    }
-  };
+  if (name === '' || surname === '' || email === '' || password === '') {
+    loginError.innerHTML = 'Preencha os campos em branco';
+  } else if (password !== passwordConfirm) {
+    loginError.innerHTML = 'Senha inválida';
+  } else if (password.length < 6) {
+    loginError.innerHTML = 'A senha deve possuir ao menos 6 caracteres';
+  } else if (password === passwordConfirm) {
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then((teste) => {
+      console.log(teste);
+      saverUserData();
+      window.location = '#login';
+    }).catch((error) => {
+      const errorCode = error.code;
+      console.log(error);
+      
+    });
+  }
+};
 
 const render = ()  => {
-    const container = document.createElement('div');
-    container.innerHTML = template;
-    return container;
+  const container = document.createElement('div');
+  container.innerHTML = template;
+  return container;
 }
 
 const init = () => {
-    const registerButton = document.getElementById('register');
-    console.log(registerButton)
-    registerButton.addEventListener('click', create);
+  const registerButton = document.getElementById('register');
+  console.log(registerButton)
+  registerButton.addEventListener('click', create);
 };
 
 export default {
-    render, 
-    init,
+  render, 
+  init,
 }
