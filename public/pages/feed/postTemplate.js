cfa8b7fa450f9d68dc2bac779cf9f326920ce9e1
Sofia <sofiasimas1992@gmail.com>
2020-06-17 19:38:37 -0300

    fix code style

--- public/pages/feed/postTemplate.js
+++ public/pages/feed/postTemplate.js
@@ -1,56 +1,55 @@
 
 const showDelete = (currentUser, userId, id) => {
-    if (userId === currentUser) {
-        return `<img src="imagens/lixo-01.png" id="delete-${id}" data-id="${id}" class="delete"/>`
-    }
+  if (userId === currentUser) {
+    return `<img src="imagens/lixo-01.png" id="delete-${id}" data-id="${id}" class="delete"/>`
+  }
 
-    return ''
+  return ''
 }
 
-const commentTemplate = ({text, userId}) => {
-    return`
+const commentTemplate = ({ text, userId }) => {
+  return `
     <div>
-        <div>
-            <img src="imagens/astronautrosie.jpg" alt="" class="foto-feed">
-            <span class="user-post">${userId}</span>
-            <img src="imagens/lixo-01.png"/ class="delete"/>
-        </div>
-        <p class="post-text">${text}</p>
+      <div class="img-name">
+        <img src="imagens/astronautrosie.jpg" alt="" class="foto-comment">
+        <span class="user-post">${userId}</span>
+      </div>
+      <p class="comment-text">${text}</p>
     </div>
     `
 }
 
-const postTemplate = ({ userId, userName, text, likes, id , currentUser, comments}) => {
-    return `
-        <div class="template-feed">
-            <div class="feed-pessoal">
-                <img src="imagens/astronautrosie.jpg" alt="" class="foto-feed">
-                <span class="user-post">${userName}</span>
-            </div>
-            <div class="itens-text">
-                <p class="post-text">${text}</p>
-            </div>
-            <div class="itens-post">
-                <div class="icons-left"> 
-                    <img src="imagens/like-01.png" id="like-${id}" data-id="${id}" class="like-feed"/>${likes}
-                    ${showDelete(currentUser, userId, id)}
-                </div>
-                <div class="icons-right">
-                    <img src="imagens/comentar.png" id="comments-${id}" class="comentar-feed"/>
-                    <img src="imagens/calendar.png" id="calendar" class="calendar-feed"/>
-                    <img src="imagens/compartilhar.png" id="compartilhar" class="compartilhar-feed"/>
-                </div>
-                <div id="newCommentContainer-${id}" class="comment-container">
-                    <div class="comment">
-                        <img src="imagens/astronautrosie.jpg" alt="" class="foto-comment">
-                        <input type="text" data-id="${id}" id="addComment-${id}" class="input-comment"/>
-                    </div>
-                </div>
-                <div id="newCommentContainer-${id}" class="comment-container show">
-                    ${comments.map(commentTemplate).join('')}
-                </div>
-            </div>
-        </div>`
+const postTemplate = ({ userId, userName, text, likes, id, currentUser, comments }) => {
+  return `
+    <div class="template-feed">
+      <div class="feed-pessoal">
+        <img src="imagens/astronautrosie.jpg" alt="" class="foto-feed">
+        <span class="user-post">${userName}</span>
+      </div>
+      <div class="itens-text">
+        <p class="post-text">${text}</p>
+      </div>
+      <div class="itens-post">
+        <div class="icons-left"> 
+          <img src="imagens/like-01.png" id="like-${id}" data-id="${id}" class="like-feed"/>${likes}
+          ${showDelete(currentUser, userId, id)}
+        </div>
+        <div class="icons-right">
+          <img src="imagens/comentar.png" id="comments-${id}" class="comentar-feed"/>
+          <img src="imagens/calendar.png" id="calendar" class="calendar-feed"/>
+          <img src="imagens/compartilhar.png" id="compartilhar" class="compartilhar-feed"/>
+        </div>
+      </div>
+      <div id="newCommentContainer-${id}" class="comment-container">
+        <div class="comment">
+          <img src="imagens/astronautrosie.jpg" alt="" class="foto-comment">
+          <input type="text" data-id="${id}" id="addComment-${id}" class="input-comment"/>
+        </div>
+      </div>
+      <div class="comment-container show">
+        ${comments.map(commentTemplate).join('')}
+      </div>
+    </div>`
 }
 
 export default postTemplate;
--- public/router/index.js
+++ public/router/index.js
@@ -5,31 +5,31 @@ const pageContainer = document.getElementById('page-container');
 const validateHash = (hash) => hash === '' ? 'login' : hash.replace('#', '');
 
 const renderPage = () => {
-    pageContainer.innerHTML='';
-    const page = validateHash(window.location.hash);
-    const route = routes[page];
-    if (page != 'login') {
-        firebase.auth().onAuthStateChanged(function (user) {
-            if (user) {
-                pageContainer.appendChild(route.render());
-                route.init();
-            } else {
-                document.location.hash = '#login';
-            }
-        });
-    } else {
+  pageContainer.innerHTML = '';
+  const page = validateHash(window.location.hash);
+  const route = routes[page];
+  if (page != 'login') {
+    firebase.auth().onAuthStateChanged(function (user) {
+      if (user) {
         pageContainer.appendChild(route.render());
         route.init();
-    }
+      } else {
+        document.location.hash = '#login';
+      }
+    });
+  } else {
+    pageContainer.appendChild(route.render());
+    route.init();
+  }
 }
 
 const router = () => {
-    window.addEventListener('load', () => {
-        renderPage();
-    });
-    window.addEventListener('hashchange', () => {
-        renderPage();
-    })
+  window.addEventListener('load', () => {
+    renderPage();
+  });
+  window.addEventListener('hashchange', () => {
+    renderPage();
+  })
 }
 
 export default router;
