0b6c3c455b1ff0dc4fcdb4288263cc2b95b743f3
Sofia <sofiasimas1992@gmail.com>
2020-06-17 10:59:05 -0300

    add comment

--- public/pages/feed/index.js
+++ public/pages/feed/index.js
@@ -21,17 +21,33 @@ const createPost = () => {
         likes:0,
         private: privatePost,
         created: new Date(),
+        comments:[],
     }
     firebase.firestore().collection('posts').add(post);
 }
 
-const likePost =() => {
+const likePost = () => {
     const id = event.target.dataset.id;
     firebase.firestore().collection('posts').doc(id).get()
     .then((post) => {
       const likes = post.data().likes + 1;
       firebase.firestore().collection('posts').doc(id).update({
         likes,
+      });
+    });
+}
+
+const commentPost = (id, text) => {
+    firebase.firestore().collection('posts').doc(id).get()
+    .then((post) => {
+      const comments = post.data().comments;
+      const userId = firebase.auth().currentUser.uid
+      const comment = {text, userId}
+      
+      comments.push(comment)
+      console.log(comments);
+      firebase.firestore().collection('posts').doc(id).update({
+        comments,
       });
     });
 }
@@ -53,23 +69,49 @@ const addLikeEvent = (post) => {
     likeButton.addEventListener('click', likePost)
 }
 
+const showCommentButton = (post) => {
+    const commentButton = document.getElementById(`comments-${post.id}`)
+    commentButton.addEventListener('click',() => {
+        const newCommentContainer = document.getElementById(`newCommentContainer-${post.id}`)
+        newCommentContainer.classList.toggle('show')
+    })
+}
+
+const addComment = (post) => {
+    const inputEnter = document.getElementById(`addComment-${post.id}`);
+    inputEnter.addEventListener('keyup', function(e){
+        var key = e.which || e.keyCode;
+        if (key == 13) { // codigo da tecla enter
+        // colocas aqui a tua função a rodar
+            commentPost(post.id, inputEnter.value)
+        }
+    });
+}
+
 const addEvents = (post) => {
     addLikeEvent(post);
     addDeleteEvent(post);
+    showCommentButton(post);
+    addComment(post);
 }
-
 
 const showFeed = () => {
     const currentUser = firebase.auth().currentUser.uid
     const timeline = document.getElementById('timeline')
-    firebase.firestore().collection("posts").orderBy('created', 'desc')
+    firebase.firestore()
+    .collection("posts")
+    .orderBy('created', 'desc')
     .onSnapshot(function(querySnapshot) {
         var posts = [];
         querySnapshot.forEach(function(doc) {
             const post = doc.data()
             post.id = doc.id
             post.currentUser = currentUser
-            posts.push(post);
+            if(post.private && post.userId===currentUser){
+                posts.push(post);
+            } else if(!post.private){
+                posts.push(post)
+            }
         });
         
         const content = posts.map(postTemplate)
