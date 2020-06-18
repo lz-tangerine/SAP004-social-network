75930838bcda0c4d7391f6fbfaf035a3d8ea13ae
karina1981 <karina.souza1981@gmail.com>
2020-06-18 18:26:14 -0300

    Logout

---
+++ public/pages/logout/index.js
@@ -0,0 +1,20 @@
+const render = ()  => {
+  const container = document.createElement('div');
+
+  container.innerHTML = '';
+
+  return container;
+}
+
+const init =  () => {
+    firebase.auth().signOut().then(function() {
+        window.location.hash = '#login';
+    }).catch(function(error) {
+        window.location.hash = '#login';
+    });
+}
+
+export default {
+  render,
+  init,
+}
