3954cda5b67210eed5270a1cfc3d7c750f785852
SofiaSimas <32040895+SofiaSimas@users.noreply.github.com>
2020-06-25 07:51:33 -0300

    Revert "atualizações"

--- public/router/routes.js
+++
@@ -1,9 +0,0 @@
-import login from '../pages/login/index.js';
-import register from '../pages/register/index.js';
-
-const routes = {
-    login: login(), 
-    register: register()
-}
-
-export default routes;
